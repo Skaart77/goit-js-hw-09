7b50d445eed726dede79092b749eb89e7b1bd692
Skaart77 <114434801+Skaart77@users.noreply.github.com>
2023-03-20 11:20:13 +0000

    Deploying to gh-pages from @ Skaart77/goit-js-hw-09@3367f4a7192584c48fdb1599ac06811f39df8597 🚀

--- 01-color-switcher.11db52a0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){return e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,n.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.11db52a0.js.map
---
+++ 01-color-switcher.a9974453.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){a=setInterval((()=>e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,d.disabled=!0}));let a=null;
+//# sourceMappingURL=01-color-switcher.a9974453.js.map
